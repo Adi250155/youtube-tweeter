bef8819b63ab8e9f69f2dadcd77b3c5a656f0eb3
Aadi <adiashu1787@gmail.com>
2026-09-25 12:53:05 +0530

    completed dashboard controller

--- src/controllers/dashboard.controller.js
+++ src/controllers/dashboard.controller.js
@@ -8,31 +8,48 @@ import {asyncHandler} from "../utils/asyncHandler.js"
 
 const getChannelStats = asyncHandler(async (req, res) => {
     // TODO: Get the channel stats like total video views, total subscribers, total videos, total likes etc.
-    /*
-    Get logged-in user's ID
-        ↓
-Find all videos where owner = user ID
-        ↓
-Count videos
-        ↓
-Add views of those videos
-        ↓
-Find subscriptions where channel = user ID
-        ↓
-Count subscribers
-        ↓
-Find likes belonging to those videos
-        ↓
-Count likes
-        ↓
-Return all stats 
-    */
+      const userId =req.user._id
+      const videos=await Video.find({owner:userId})
+      const totalVideos =videos.length
+      const totalViews =videos.reduce((sum,video)=>{
+        return sum + video.views
+      },0)
 
+      const totalSubscriber = await Subscription.countDocuments({
+        channel:userId
+      })
         
+      const videoIds = videos.map(video => video._id)
+      const totalLikes = await Like.countDocuments({
+        video:{$in:videoIds}
+    })
+
+    const stats = {
+        totalVideos,
+        totalViews,
+        totalSubscribers,
+        totalLikes
+    }
+
+    return res.status(200).json(
+        new ApiResponse(200,stats,"Channel stats fetched successfully")
+    )
 })
 
 const getChannelVideos = asyncHandler(async (req, res) => {
     // TODO: Get all the videos uploaded by the channel
+    const userId = req.user._id
+
+    const videos = await Video.find({
+        owner:userId
+    })
+    .populate("owner","username avatar")
+    .sort({createdAt:-1})
+
+    return res.status(200).json(
+        new ApiResponse(200,videos,"Channel videos fetched successfully")
+    )
+
 })
 
 export {
