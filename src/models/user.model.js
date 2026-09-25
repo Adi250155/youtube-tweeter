f5301bac69ca384da46cb1ff4bb797d0a731ea66
Aadi <adiashu1787@gmail.com>
2026-09-25 17:12:30 +0530

    bugs fixed

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -20,7 +20,7 @@ const userSchema =new Schema(
         trim: true,
        
       } ,
-      fullname:{
+      fullName:{
           type: String,
          required:true,
            trim: true,
@@ -54,11 +54,11 @@ const userSchema =new Schema(
 
 )
 
-userSchema.pre("save", async function (next) {
-    if(!this.isModified("password")) return next();
+userSchema.pre("save", async function () {
+    if(!this.isModified("password")) return 
 
     this.password = await bcrypt.hash(this.password, 10)
-    next()
+    
 })
 
 userSchema.methods.isPasswordCorrect = async function(password){
