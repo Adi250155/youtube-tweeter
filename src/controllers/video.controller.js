27e45eb4e329bdcf2066f22d7e18c4fe755c9993
Aadi <adiashu1787@gmail.com>
2026-09-25 12:42:56 +0530

    togglePub;ishStatus added in video controller

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -193,9 +193,34 @@ const deleteVideo = asyncHandler(async (req, res) => {
 })
 
 const togglePublishStatus = asyncHandler(async (req, res) => {
-    const { videoId } = req.params
 
+    const {videoId} = req.params
 
+    if (!isValidObjectId(videoId)) {
+        throw new ApiError(400, "Invalid video ID")
+    }
+
+    const video = await Video.findById(videoId)
+
+    if (!video) {
+        throw new ApiError(404, "Video not found")
+    }
+
+    if (video.owner.toString() != req.user._id.toString()) {
+        throw new ApiError(403, "You are not allowed to change this video's status")
+    }
+
+    video.isPublished = !video.isPublished
+
+    await video.save()
+
+    return res.status(200).json(
+        new ApiResponse(
+            200,
+            video,
+            "Video publish status updated successfully"
+        )
+    )
 })
 
 export {
